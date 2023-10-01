84e0fd3858565559c8f4b9e76e7ca5302f44fe42
Henry Dao <mhoang0102@gmail.com>
2023-10-01 13:16:28 -0700

    add main.js

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ let nav = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec =>{
         let top = window.scrollY;
-        let offset = sec.offsetTop -100;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
         if(top >= offset && top < offset + height){
             nav.forEach(link => {
                 link.classList.remove('current');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('current');
-            })
+            });
         }
-    })
+    });
 }
